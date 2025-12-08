61a38ab785353182739ad21c9ee050209c5edca3
vairavel <vairavel@kssmart.co>
2025-12-08 16:45:22 +0530

    css flicker solved

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"], 
-  format: ["esm", "cjs"],
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  minify: true,
-  target: "esnext",
-  injectStyle: true, // THIS makes styles auto-inject
-  splitting: false,
+   entry: ['src/index.ts'],
+    dts: true,
+    clean: true,
 });
